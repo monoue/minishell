b348a022216e7166adf84dfc60d6f945f4bc31b8
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2021-02-15 16:55:44 +0900

    is_quoted_wrongly fixing

--- src/syntax_error_check/is_quoted_wrongly.c
+++ src/syntax_error_check/is_quoted_wrongly.c
@@ -9,7 +9,28 @@ bool	is_quoted_wrongly(char *str)
 	index = 0;
 	while (index < len)
 	{
-		// if (is_quote_not_escaped(str, index))
+		if (str[index] == '\\')
+		{
+			if (is_quote_char(str[index + 1]))
+			{
+				quote = str[index + 1];
+				index += 2;
+				while (index < len)
+				{
+					if (str[index] == '\\')
+					{
+						if (str[index + 1] == quote)
+						{
+							index += 2;
+							break ;
+						}
+						index += 2;
+					}
+				}
+			}
+			else
+				index += 2;
+		}
 		if (is_specific_char_not_escaped(str, index, is_quote_char))
 		{
 			quote = str[index];
