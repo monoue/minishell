efab50bc0a806a9475e1434f8c1505e9eed59b25
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2021-02-16 08:57:08 +0900

    is_escaped fixed: now can handle "\\"

--- src/parser/is_escaped.c
+++ src/parser/is_escaped.c
@@ -2,7 +2,17 @@
 
 bool	is_escaped(const char *str, size_t index)
 {
+	size_t	b_slash_num;
+	int		i;
+	
 	if (index == 0)
 		return (false);
-	return (str[index - 1] == '\\');
+	i = index - 1;
+	b_slash_num = 0;
+	while (i >= 0 && str[i] == '\\')
+	{
+		b_slash_num++;
+		i--;
+	}
+	return (b_slash_num % 2);
 }
