93ffc4adf3f3f889662103ab69c134a3c77a92c7
monoue <like_a_newborn_baby@icloud.com>
2021-03-01 11:24:12 +0900

    fd designation seems to be working perfectly

--- src/parser/process_one_command.c
+++ src/parser/process_one_command.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 14:45:35 by monoue            #+#    #+#             */
-/*   Updated: 2021/02/26 14:02:29 by monoue           ###   ########.fr       */
+/*   Updated: 2021/03/01 11:22:57 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static int	get_child_process_result(int status)
 	return (ERROR);
 }
 
-static void	fork_exec_commands(char **piped_chunks, t_list *envp)
+static void	exec_no_pipe_chunk(char *chunk, t_list *envp)
 {
 	int		status;
 	pid_t	pid;
@@ -31,27 +31,12 @@ static void	fork_exec_commands(char **piped_chunks, t_list *envp)
 		exit_err_msg(strerror(errno));
 	if (pid == 0)
 	{
-		process_pipes(piped_chunks, 0,
-							ft_count_strs((const char**)piped_chunks), envp);
-		ft_free_split(piped_chunks);
+		exec_command_chunk(chunk, envp, true);
 		exit(EXIT_SUCCESS);
-		// exit(g_last_exit_status);
 	}
 	g_pid = pid;
 	wait(&status);
 	g_last_exit_status = get_child_process_result(status);
-}
-
-static void	exec_no_pipe_chunk(char **chunks, t_list *envp)
-{
-	char	**chunk_words;
-
-	chunk_words = split_command_line(chunks[0]);
-	if (is_reproduction(chunk_words[0]))
-		exec_command_chunk(chunks[0], envp, false);
-	else
-		fork_exec_commands(chunks, envp);
-	ft_free_split(chunk_words);
 }
 
 void		process_one_command(char *command, t_list *envp)
@@ -64,7 +49,7 @@ void		process_one_command(char *command, t_list *envp)
 		exit_err_msg(MALLOC_ERR);
 	chunks_num = ft_count_strs((const char**)piped_chunks);
 	if (chunks_num == 1)
-		exec_no_pipe_chunk(piped_chunks, envp);
+		exec_no_pipe_chunk(piped_chunks[0], envp);
 	else if (chunks_num >= 2)
 		has_pipe(piped_chunks, envp, chunks_num);
 	ft_free_split(piped_chunks);
--- src/parser/process_pipes.c
+++ src/parser/process_pipes.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 14:44:43 by monoue            #+#    #+#             */
-/*   Updated: 2021/02/17 14:44:44 by monoue           ###   ########.fr       */
+/*   Updated: 2021/03/01 11:12:17 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,33 @@
 void	process_pipes(char **piped_chunks, size_t i, size_t chunks_num,
 																t_list *envp)
 {
-	int		fds[2];
+	(void)chunks_num;
+	(void)i;
 
-	if (i == chunks_num - 1)
-	{
+
+	// int		fds[2];
+
+	// if (i == chunks_num - 1)
+	// {
 		exec_command_chunk(piped_chunks[0], envp, true);
-		return ;
-	}
-	pipe(fds);
-	if ((g_pid = fork()) == 0)
-	{
-		close(fds[0]);
-		close(STDOUT_FILENO);
-		dup2(fds[1], STDOUT_FILENO);
-		close(fds[1]);
-		process_pipes(piped_chunks, i + 1, chunks_num, envp);
-	}
-	else
-	{
-		close(fds[1]);
-		close(STDIN_FILENO);
-		dup2(fds[0], STDIN_FILENO);
-		close(fds[0]);
-		exec_command_chunk(piped_chunks[(chunks_num - 1) - i], envp, true);
-	}
-	ft_free_split(piped_chunks);
+	// 	return ;
+	// }
+	// pipe(fds);
+	// if ((g_pid = fork()) == 0)
+	// {
+	// 	close(fds[0]);
+	// 	close(STDOUT_FILENO);
+	// 	dup2(fds[1], STDOUT_FILENO);
+	// 	close(fds[1]);
+	// 	process_pipes(piped_chunks, i + 1, chunks_num, envp);
+	// }
+	// else
+	// {
+	// 	close(fds[1]);
+	// 	close(STDIN_FILENO);
+	// 	dup2(fds[0], STDIN_FILENO);
+	// 	close(fds[0]);
+	// 	exec_command_chunk(piped_chunks[(chunks_num - 1) - i], envp, true);
+	// }
+	// ft_free_split(piped_chunks);
 }
