cd03e70bf3d3db962d9c5243c63b3499d78fbd4e
monoue <like_a_newborn_baby@icloud.com>
2021-03-08 14:32:55 +0900

    exit no-option (--) done

--- src/command/exit_command.c
+++ src/command/exit_command.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 17:41:05 by sperrin           #+#    #+#             */
-/*   Updated: 2021/03/03 09:56:59 by monoue           ###   ########.fr       */
+/*   Updated: 2021/03/08 14:31:30 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,14 @@ static void	exit_minishell2(int argv_num, char **arg, bool pipe_child)
 	int	nbr;
 	int	exit_nbr;
 
+	if (!ft_strlen(arg[1]) || !str_is_valid_num(arg[1]))
+	{
+		put_error_numeric(arg[1]);
+		if (!pipe_child)
+			put_farewell_greeting();
+		ft_free_split(arg);
+		exit(255);
+	}
 	if (argv_num > 2)
 	{
 		put_bash_err_msg("exit", "too many arguments");
@@ -81,27 +89,26 @@ static void	exit_minishell2(int argv_num, char **arg, bool pipe_child)
 
 void		exit_minishell(char **argv, t_list *envp, bool pipe_child)
 {
-	char	**arg;
-	size_t	arg_num;
+	char		**arg;
+	size_t		arg_num;
+	const char	*no_option = "--";
 
 	arg = set_command_argv(argv, envp);
 	arg_num = ft_count_strs((const char **)arg);
 	if (!pipe_child)
 		ft_putendl_err("exit");
-	if (arg_num == 1)
+	if (arg_num == 1 || (arg_num == 2 && ft_strequal(arg[1], no_option)))
 	{
 		if (!pipe_child)
 			put_farewell_greeting();
 		ft_free_split(arg);
 		exit(g_last_exit_status);
 	}
-	if (!ft_strlen(arg[1]) || !str_is_valid_num(arg[1]))
+	if (ft_strequal(arg[1], no_option))
 	{
-		put_error_numeric(arg[1]);
-		if (!pipe_child)
-			put_farewell_greeting();
-		ft_free_split(arg);
-		exit(255);
+		SAFE_FREE(arg[1]);
+		arg[1] = ft_strdup(arg[2]);
+		arg_num--;
 	}
 	exit_minishell2(arg_num, arg, pipe_child);
 }
