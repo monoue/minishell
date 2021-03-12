952b12d647e4bddf133b9678bad0cb762587ad4f
monoue <like_a_newborn_baby@icloud.com>
2021-03-12 15:39:31 +0900

    norm done

--- src/command/cd_command.c
+++ src/command/cd_command.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 17:40:49 by sperrin           #+#    #+#             */
-/*   Updated: 2021/03/12 15:32:04 by monoue           ###   ########.fr       */
+/*   Updated: 2021/03/12 15:36:38 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,30 +75,6 @@ static void	put_error(char *argv)
 	ft_putstr_err(argv);
 	ft_putstr_err(": ");
 	ft_putendl_err(strerror(errno));
-}
-
-static char	*find_home(t_list *envp)
-{
-	const char	*home_key = "HOME=";
-	char		*variable;
-	int			count;
-	char		*str;
-
-	str = NULL;
-	while (envp && envp->next)
-	{
-		if (ft_strnequal((char*)envp->content, home_key, ft_strlen(home_key)))
-			variable = ft_strdup((char*)envp->content);
-		envp = envp->next;
-	}
-	if (variable != NULL)
-	{
-		count = ft_strrchr_int(variable, '=');
-		str = ft_substr_free(variable, count + 1, ft_strlen(variable) - count);
-	}
-	if (str_is_of_tabs_or_spaces(str))
-		SAFE_FREE(str);
-	return (str);
 }
 
 void		cd(char **argv, t_list *envp, char *home_key)
